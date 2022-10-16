6ba59bec1cc0716fa9f35ff2441cf60daf973e88
Jmarlow <45525109+Jemarlow@users.noreply.github.com>
2022-10-16 16:04:57 +0100

    extend primes function - first version

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
     list = []
+
+    for n in range(2, 2 + number_of_primes):
+        for i in range(2, n):
+
+            isPrime = n%i != 0
+
+            if isPrime is False:
+                break
+
+        if isPrime:
+            list.append(n)
+
     return list
